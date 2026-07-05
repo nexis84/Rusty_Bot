76acc29f3a71878dfb23fa80d4024657d4b6c9af
Adam <nexisenb@googlemail.com>
2026-07-05 23:58:29 +0100

    Update version display to v1.0.1

--- SleeperArchive/src/App.tsx
+++ SleeperArchive/src/App.tsx
@@ -2721,7 +2721,7 @@ export default function App() {
 
       {/* Version */}
       <div className="fixed bottom-2 left-2 text-[9px] font-mono text-white/20 select-none z-50 pointer-events-none">
-        v1.0.0
+        v1.0.1
       </div>
 
       {/* Background Nebula Overlay */}
